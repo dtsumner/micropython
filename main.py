7e68e26d10594a383c9c28bd50f78d4ec79f7642
dtsumner <44790515+dtsumner@users.noreply.github.com>
2025-10-29 00:27:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-#New update
-print("Hello, OTA was a success!")
+from machine import Pin
+from time import sleep
+
+led = Pin(2, Pin.OUT)
+state = 0
+while True:
+    led.value(not led.value())
+    if(led.value() == 1):
+        print("Led is On")
+    else:
+        print("Led is Off")
+    
+    sleep(0.5)
